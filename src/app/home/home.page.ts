49b7c37dd00816dfa6d9d18977266c7ddd33f222
Adrian Vargas <adrianfelipevr@gmail.com>
2025-05-24 18:54:00 -0500

    Products Style, and Loading animation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  loaded: boolean = false;
+  products: any[] = [];
+  constructor(
+    private productService: ProductsService,
+    private loadingController: LoadingController
+  ) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.loadingProducts();
+  }
 
+  async loadingProducts() {
+    const loading = await this.loadingController.create({
+      message: 'Loading products...',
+    });
+    loading.present();
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+      console.log(this.products);
+      this.loaded = true;
+      loading.dismiss();
+    });
+  }
 }
